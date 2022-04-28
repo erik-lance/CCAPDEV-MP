ff433df16330332137be3f39e837d2ba5b85028a
erik-lance <eriklance@gmail.com>
2022-04-28 13:38:00 +0800

    Button functions for hovering

---
+++ scripts/ui-button.js
@@ -0,0 +1,23 @@
+function upvoteHover(x) {
+    x.src="images/ui/upvote-filled.png"
+}
+
+function upvoteLeave(x) {
+    x.src="images/ui/upvote-hollow.png"
+}
+
+function downvoteHover(x) {
+    x.src="images/ui/downvote-filled.png"
+}
+
+function downvoteLeave(x) {
+    x.src="images/ui/downvote-hollow.png"
+}
+
+function shareHover(x) {
+    x.src="images/ui/share-filled.png"
+}
+
+function shareLeave(x) {
+    x.src="images/ui/share-hollow.png"
+}
