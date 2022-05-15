e52e24cee29f467a4ffb3d07c77199a80ce2c5b2
erik-lance <eriklance@gmail.com>
2022-05-15 23:38:57 +0800

    Removed unnecessary functions, made upvote
    Currently does nothing.

--- scripts/ui-button.js
+++ scripts/ui-button.js
@@ -1,23 +1,5 @@
-function upvoteHover(x) {
-    x.src="images/ui/upvote-filled.png"
-}
-
-function upvoteLeave(x) {
-    x.src="images/ui/upvote-hollow.png"
-}
-
-function downvoteHover(x) {
-    x.src="images/ui/downvote-filled.png"
-}
-
-function downvoteLeave(x) {
-    x.src="images/ui/downvote-hollow.png"
-}
-
-function shareHover(x) {
-    x.src="images/ui/share-filled.png"
-}
-
-function shareLeave(x) {
-    x.src="images/ui/share-hollow.png"
+function upvote(x) {
+    var vsvg = x.querySelector(svg);
+    window.alert(vsvg)
+    x.svg.path.style.fill = "#fb8500";
 }
