da33b149a226af7eddb083533afbb74167cdaaf8
erik-lance <eriklance@gmail.com>
2022-05-20 22:23:30 +0800

    Made navbar CORS compatible by writing inner HTML manually

--- scripts/navbar-init.js
+++ scripts/navbar-init.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    $("#nav-bar").load("pages/nav.html");
+    const navbar = `
+        <div class="top_navbar">
+        <div id="nav-icon">   
+            <img src="images/web-logo-white.png" style="max-width:100%; max-height:100%;" />
+        </div>
+        
+        <a href="index.html" class="active">Home</a>
+        <div id="search-bar">
+            <input type="text" placeholder="Find">
+        </div>
+        <div id="top-profile">
+            <a href="pages/user-sign.html">Profile</a>
+        </div>
+        </div>
+    `;
+
+    $("#nav-bar").html(navbar);
 });
