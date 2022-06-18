fe5a9ece54cc1447bce8ed22fcdd8e36d5f5c56f
Michael-Mangoba <99485815+Michael-Mangoba@users.noreply.github.com>
2022-06-18 16:09:46 +0800

    added db.js
    This database manager is based on sir in the mini challenge but with the added models. Proper database manipulation will depend on the controller part of the system

---
+++ models/db.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose');
+
+const userModel = require('./Schemas/user.js');
+const filesModel = require('./Schemas/files.js');
+const postModel = require('./Schemas/post.js');
+const puzzleModel = require('./Schemas/puzzle.js');
+const commentModel = require('./Schemas/comment.js');
+
+const url = 'mongodb://localgost:27017/CCAPDEV_MP';
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParse: true
+};
+
+const database = {
+
+    connect: function(){
+        mongoose.connect(url, options, function(error){
+            if(error) throw error;
+            console.log('Connected to: ' + url);
+        });
+    },
+    
+    insertOne: function(model, doc, callback){
+        model.create(doc, function(error, result){
+            if(error) return callback(false);
+            console.log('Added ' + result);
+            return callback(true);
+        });
+    },
+
+    insertMany: function(model, docs, callback){
+        model.insertMany(docs, function(error, result){
+            if(error) return callback(false);
+            console.log('Added ' + result);
+            return callback(true);
+        });
+    },
+
+    findOne: function(model, query, projection, callback){
+        model.findOne(query, projection, function(error, result){
+            if(error) return callback(false);
+            return callback(result);
+        });
+    },
+
+    findMany: function(model, query, projection, callback){
+        model.findMany(query, projection, function(error, result){
+            if(error) return callback(false);
+            return callback(result);
+        });
+    },
+
+    updateOne: function(model, filter, update, callback){
+        model.updateOne(filter, update, function(error, result){
+            if(error) return callback(false);
+            console.log('Document modified: ' + result.nModified);
+            return callback(true);
+        });
+    },
+
+    updateMany: function(model, filter, update, callback){
+        model.updateMany(conditions, function (error, result){
+            if(error) return callback(false);
+            console.log('Document modified: ' + result.nModified);
+            return callback(true);
+        });
+    },
+
+    deleteOne: function(model, conditions, callback) {
+        model.deleteOne(conditions, function (error, result) {
+            if(error) return callback(false);
+            console.log('Document deleted: ' + result.deletedCount);
+            return callback(true);
+        });
+    },
+
+    deleteMany: function(model, conditions, callback) {
+        model.deleteMany(conditions, function (error, result) {
+            if(error) return callback(false);
+            console.log('Document deleted: ' + result.deletedCount);
+            return callback(true);
+        });
+    }
+}
+
+module.exports = database;
