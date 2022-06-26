a268f2f9623827a9585063a80391a4f9faf8a031
erik-lance <eriklance@gmail.com>
2022-06-26 23:58:57 +0800

    Added guide

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,5 +1,13 @@
 $('#nav-search').click(function() {
 
-
+    // Ajax call to search (no route yet, make one maybe?)
+    // searches for posts based on title!
+    /*
+        $.get('')
     
+    
+    */
+
+    // Must return to home page
+    window.location.href="/"
 })
